2781ee7c1b09b72426acbfa1d1ee903856ebc173
Bot-Kerem <akerem1434@gmail.com>
2022-12-03 16:43:01 +0000

    02

---
+++ day01_puzzle2/main.cpp
@@ -0,0 +1,54 @@
+#include <cstddef>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <array>
+
+template <size_t size>
+void place(std::array<int, size>& top, int cal)
+{
+	for(int i = 0; i < size; i++)
+	{
+		if(cal > top[i])
+		{
+			top[i] = cal;
+			break;
+		}
+	}
+}
+
+template <size_t size>
+int sum(std::array<int, size> arr)
+{
+	int total = 0;
+	for(int i = 0; i < size; i++)
+	{
+		total += arr[i];
+	}
+	return total;
+}
+
+int main()
+{
+	std::ifstream input("./input.txt");
+
+	std::string line;
+	int current = 0;
+	std::array<int, 3> top;
+	while(std::getline(input, line))
+	{
+		if(line.length() == 0)
+		{
+			place(top, current);
+			current = 0;
+		}
+		current += std::atoi(line.c_str());
+	}
+	place(top, current);
+
+	int top_sum = sum(top);
+	std::cout << "Total " << top_sum << " calories that 3 elves carrying\n";
+
+	return EXIT_SUCCESS;
+}
